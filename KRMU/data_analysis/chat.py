f3b7c44e597f596bdda5539ea0c991dd6f73f403
devkartikrathi <kartikrathi0808@gmail.com>
2023-02-15 18:05:59 +0530

    openai file added

---
+++ KRMU/data_analysis/chat.py
@@ -0,0 +1,28 @@
+import openai
+import pandas as pd
+import ast
+
+with open("key.txt") as f:
+    KEY = str(f.readlines()[0])
+
+openai.api_key = KEY
+
+class coder:
+
+    def __init__(self, data, inst, col_name):
+        self.df = data
+        self.instruction = inst
+        self.columns = col_name
+
+    def _code(self):
+        column_info = " ".join([f"{column}: float" for column in self.columns])
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=f"Generate only code that can be run using a single python exec function with all code is seperated with ';' to perform the following data analysis on a pandas dataframe named as df file and always add print function to output with correct notation: {self.instruction}\n\nColumn information: {column_info}",
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        code = response["choices"][0]["text"]
+        return code
